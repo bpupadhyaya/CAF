9ef762dee25e11e65cc2844422b043f9e205062d
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2015-09-25 16:00:36 -0700

    Initial files for caf scala project.

---
+++ caf-scala/src/main/scala/com/equalinformation/caf/scala/TestObject.scala
@@ -0,0 +1,9 @@
+package com.equalinformation.caf.scala
+
+/**
+ * Created by bpupadhyaya on 9/25/15.
+ */
+object TestObject extends App {
+  println("test object")
+
+}
---
+++ caf-scala/src/test/scala/com/equalinformation/caf/test/scala/TestObject.scala
@@ -0,0 +1,8 @@
+package com.equalinformation.caf.test.scala
+
+/**
+ * Created by bpupadhyaya on 9/25/15.
+ */
+object TestObject {
+
+}
